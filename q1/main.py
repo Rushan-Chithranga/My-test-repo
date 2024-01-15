14e12cf4ca52d2dca031bce0c75ea7d60095bce5
Chamika Edirisinghe <edirisinghe@thesanmark.com>
2024-01-15 10:58:34 +0530

    Question 01

---
+++ q1/main.py
@@ -0,0 +1,21 @@
+import json
+
+with open('./data2.json', 'r') as f:
+  dice = json.load(f)
+
+def transform(dice):
+    try:
+        rotatedDice = []
+        for i in range(0, len(dice)):
+            newRow = []
+            for j in range(len(dice[i])-1, -1, -1):
+                newRow.append(dice[j][i])
+            rotatedDice.append(newRow)
+        return rotatedDice
+    except Exception as e:
+        return "Invalid input"
+
+rotatedDice = transform(dice)
+
+with open("output.json", "w") as outfile:
+    outfile.write(json.dumps(rotatedDice))
